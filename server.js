1f5bbcb036fb4105d86fb72dce0c9c4f0340b742
Kretz <joaobernardoc@hotmail.com>
2020-10-19 02:16:38 +0100

    Added server libraries, express, expresslify, serve-static,connect-history-api-fallback

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const serveStatic = require('serve-static')
+const history = require('connect-history-api-fallback')
+const enforce = require('express-sslify')
+
+const app = express();
+
+app.use(enforce.HTTPS({ trustProtoHeader:true }))
+//build all packages
+app.use(serveStatic(__dirname + '/dist'))
+
+//stop history issues
+app.use(history())
+
+
+
+app.listen(process.env.PORT || 5000)
